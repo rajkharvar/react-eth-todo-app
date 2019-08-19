1b59feca4f593e8222bb125ca3269b2b0f74497d
rpk79 <rajkharvar1808@gmail.com>
2019-08-19 19:40:04 +0530

    able to add todo on blockchain

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import Web3 from 'web3';
+import contract from './contract.json';
+
+const contractAddress = '0xb8f2caa6c1d00ad46f8a6c8c183881ee3a86440a';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,18 +13,25 @@ class App extends React.Component {
       network: ''
     };
   }
-  componentDidMount() {
+  componentWillMount() {
     this.loadBlockchainNetwork();
     console.log(`${this.state.account} ${this.state.network}`);
   }
 
   // This loads blockchain network details
   async loadBlockchainNetwork() {
-    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545/');
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545/');
     const network = await web3.eth.net.getNetworkType();
     const accounts = await web3.eth.getAccounts();
-    console.log(accounts);
     this.setState({ account: accounts[0], network });
+    const todoList = new web3.eth.Contract(contract, contractAddress);
+    this.setState({ todoList });
+    todoList.methods
+      .addTodo('code')
+      .send({ from: this.state.account })
+      .once('receipt', receipt => {
+        console.log(receipt);
+      });
   }
   render() {
     return (
